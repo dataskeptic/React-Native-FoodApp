5b15203120389e21639871a6d23a791b47226b22
fullpistoladev <mateus_henrique2011@outlook.com>
2021-05-17 19:11:03 -0300

    top

--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,95 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { 
+      Text,
+      View,
+      SafeAreaView,
+      StyleSheet,
+      TouchableOpacity,
+      Image,
+      FlatList,
+      ViewPropTypes
+    } from 'react-native';
+import { COLORS, icons, images, SIZES, FONTS } from '../constants';
 
 const Home = () => {
+  
+  function renderHeader() {
+    return (
+      <View style={{ flexDirection: 'row', height: 50, paddingTop: 10}}>
+        <TouchableOpacity
+          style={{
+            width: 50,
+            paddingLeft: SIZES.padding * 2,
+            justifyContent: 'center'
+          }}
+        >
+          <Image 
+            source={icons.nearby}
+            resizeMode="contain"
+            style={{
+              width: 30,
+              height: 30
+            }}
+          />
+        </TouchableOpacity>
+
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <View
+              style={{
+                width: '70%',
+                height: "100%",
+                backgroundColor: COLORS.lightGray3,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: SIZES.radius
+            }}
+            >
+              <Text style={{...FONTS.h3}}>Current Location</Text>
+            </View>
+        </View>
+
+        <TouchableOpacity
+          style={{
+            width: 50,
+            paddingRight: SIZES.padding * 2,
+            justifyContent: 'center'
+          }}
+        >
+          <Image 
+            source={icons.basket}
+            resizeMode="contain"
+            style={{
+              width: 30,
+              height: 30
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+    )
+  }
+  
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "transparent",
-      }}>
-      <Text>Home</Text>
-    </View>
+      <SafeAreaView style={styles.container}>
+        {renderHeader()}
+      </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      backgroundColor: COLORS.lightGray4
+  },
+  shadow: {
+      shadowColor: "#000",
+      shadowOffset: {
+          width: 0,
+          height: 3,
+      },
+      shadowOpacity: 0.1,
+      shadowRadius: 3,
+      elevation: 1,
+  }
+})
+
 export default Home;
